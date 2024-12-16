bcd41a3603a0a50cd3bfa0b6eb384e2e39d38838
Spring <baharfekriii@gmail.com>
2024-12-16 15:36:28 +0330

    Initial commit

---
+++ TMDBMovieUITests/TMDBMovieUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  TMDBMovieUITestsLaunchTests.swift
+//  TMDBMovieUITests
+//
+//  Created by Bahar on 9/26/1403 AP.
+//
+
+import XCTest
+
+final class TMDBMovieUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
